7bac6308cb4bc557a036af6dce4877d11d9c3ab6
Pedro Vasconcellos <pedrovasalmeida@gmail.com>
2021-04-13 16:49:56 -0300

    test

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SidebarDrawerProvider>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SidebarDrawerProvider>
+  );
+}
+
+export default MyApp;
